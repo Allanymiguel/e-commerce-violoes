bd2621919286092de9b8b3f13a50dff105cd8b07
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-04-06 20:40:11 -0300

    crud violoes

--- src/main/java/br/unitins/ecommerce/resource/MarcasResource.java
+++ src/main/java/br/unitins/ecommerce/resource/MarcasResource.java
@@ -18,7 +18,6 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 
 @Path("/marcas")
 @Produces(MediaType.APPLICATION_JSON)
