b8e59bf2db62eb3ea53af0929b241d893883d1b9
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-04-09 19:03:11 -0300

    fix: removing line of import

--- src/main/java/br/unitins/ecommerce/violao/repository/ViolaoAcoRepository.java
+++ src/main/java/br/unitins/ecommerce/violao/repository/ViolaoAcoRepository.java
@@ -1,7 +1,6 @@
 package br.unitins.ecommerce.violao.repository;
 
 import br.unitins.ecommerce.violao.model.ViolaoAco;
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
