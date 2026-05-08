922bbe400e4fcd27d4ef4c9119e3e226f4dd0c15
Allany Miguel Ferreira da Silva <aallanymiguel@gmail.com>
2026-05-08 18:44:26 -0300

    Imports corrigidos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,3 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
-INSERT INTO marcas (nome, pais_origem, url_website) VALUES ('Takamine', 'Japão', 'www.Takamine.com');
-INSERT INTO marcas (nome, pais_origem, url_website) VALUES ('Yamaha', 'Japão', 'www.Yamaha.com');
--- alter sequence myentity_seq restart with 4;
-
 -- Mocked data for Madeira
 INSERT INTO madeira (id, tipo, densidade, sonoridade) VALUES (1, 'Jacaranda', 'Alta', 'Brilhante');
 INSERT INTO madeira (id, tipo, densidade, sonoridade) VALUES (2, 'Cedro', 'Média', 'Aveludada');
@@ -20,11 +14,12 @@ INSERT INTO perfilbraco (id, nome, formato, espessura) VALUES (2, 'Thick', 'D', 
 INSERT INTO acessorio (id, nome, descricao, quantidade_estoque, preco_unitario) VALUES (1, 'Capotraste', 'Capotraste para violão', 50, 29.90);
 INSERT INTO acessorio (id, nome, descricao, quantidade_estoque, preco_unitario) VALUES (2, 'Correia', 'Correia de couro', 30, 49.90);
 
--- Mocked data for ViolaoAco
-INSERT INTO violoes_aco (id, nome, preco_base, ano_fabricacao, id_madeira, id_marca, id_perfil_braco, tipo_cordas_aco) VALUES (1, 'Folk Steel', 1200.00, 2022, 1, 1, 1, 'LEVE');
+-- JOINED inheritance: insert into parent table first, then child table
+INSERT INTO violao (id, nome, preco_base, ano_fabricacao, id_madeira, id_marca, id_perfil_braco) VALUES (1, 'Folk Steel', 1200.00, 2022, 1, 1, 1);
+INSERT INTO violoes_aco (id, tipo_cordas_aco) VALUES (1, 'LEVE');
 
--- Mocked data for ViolaoNylon
-INSERT INTO violoes_nylon (id, nome, preco_base, ano_fabricacao, id_madeira, id_marca, id_perfil_braco, tipo_cordas_nylon) VALUES (2, 'Clássico Nylon', 900.00, 2023, 2, 2, 2, 'MEDIA');
+INSERT INTO violao (id, nome, preco_base, ano_fabricacao, id_madeira, id_marca, id_perfil_braco) VALUES (2, 'Clássico Nylon', 900.00, 2023, 2, 2, 2);
+INSERT INTO violoes_nylon (id, tipo_cordas_nylon) VALUES (2, 'MEDIA');
 
 -- Mocked data for Violao_Acessorio (join table)
 INSERT INTO violao_acessorio (id_violao, id_acessorio) VALUES (1, 1);
